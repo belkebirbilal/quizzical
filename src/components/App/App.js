c2363ad3ea5ff953b1d9778e3b358d2b74cfd0f4
Bilal <bilalbelkebir9@gmail.com>
2024-09-13 23:07:08 +0100

    create App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import Quize from "../questions/Quize";
 import "./App.css";
 
 function App() {
+    const [take , setTake] = useState(false)
+    function handleClick() {
+        setTake(prev => !prev)
+    }
     return (
-        <div className="App"></div>
+        <div className="App">
+            <div className="take_quize" style={{display: take ? "none" : "flex"}}>
+                <div className="quizzical">
+                    <h1>Quizzical</h1>
+                    <p>Test your knowledge</p>
+                    <button onClick={handleClick}>Take Quize</button>
+                </div>
+            </div>
+            {take && <Quize />}
+        </div>
     )
 }
 
